22fa78db133b10497c1b0f7fb736c716f45ac25f
Pooya Tolideh <pooyatolideh@Pooyas-Laptop.local>
2024-07-14 00:00:51 -0400

    sorbet

--- lib/process_string.rb
+++ lib/process_string.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: false
+# typed: true
 
 # string practice
 # create a function that takes a string and subs, upper case, trim, append, reverse and prepends
+# rubocop:disable Style/MixinUsage
+extend T::Sig
 
+sig { params(str: String).returns(String) }
 def process_string(str)
   str.gsub!('e', '3')
   str.upcase!
---
+++ lib/type_interpolation.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+# typed: true
+
+def main
+  message_start = 'Happy Birthday'
+  age = 21
+  message_end = 'years old'
+  puts "#{message_start}! You are now #{age} #{message_end}"
+end
